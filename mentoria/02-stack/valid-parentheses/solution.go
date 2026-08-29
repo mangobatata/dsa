a1eb404b7f9faf42f5648597fb5233ff18bb7db2
batata <contactobrite@gmail.com>
2026-08-29 20:53:41 +0000

    stack - valid parentheses

---
+++ mentoria/02-stack/valid-parentheses/solution.go
@@ -0,0 +1,34 @@
+func isValid(s string) bool {
+	var stack []rune
+
+	for _, char := range s {
+		var lastOpen rune
+		if len(stack) > 0 {
+			lastOpen = stack[len(stack)-1]
+		}
+
+		if char == '(' || char == '[' || char == '{' {
+			stack = append(stack, char)
+		}
+
+		if char == ')' && lastOpen != '(' {
+			return false
+		} else if char == ')' && lastOpen == '(' {
+			stack = stack[:len(stack)-1]
+		}
+
+		if char == ']' && lastOpen != '[' {
+			return false
+		} else if char == ']' && lastOpen == '[' {
+			stack = stack[:len(stack)-1]
+		}
+
+		if char == '}' && lastOpen != '{' {
+			return false
+		} else if char == '}' && lastOpen == '{' {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
