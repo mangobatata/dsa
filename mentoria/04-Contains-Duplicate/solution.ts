7e7d139d20b7d4e91293cced7ce237d779dcea63
batata <contactobrite@gmail.com>
2026-08-31 17:05:52 +0000

    Contains Duplicate

--- mentoria/04-Contains-Duplicate/solution.ts
+++ mentoria/04-Contains-Duplicate/solution.ts
@@ -16,16 +16,26 @@ function containsDuplicate(nums: number[]): boolean {
   const counts: { [key: number]: number } = {};
 
   for (let i = 0; i < nums.length; i++) {
-    if (!(nums[i] in counts)) {
-      counts[nums[i]] = 1;
-    } else {
+    if (nums[i] in counts) {
       counts[nums[i]] += 1;
+    } else {
+      counts[nums[i]] = 1;
     }
 
     if (counts[nums[i]] > 1) {
       return true;
     }
   }
+
   return false;
 }
 // 29 minutos, 12 segundos
+// O(n)
+// Porque:
+// n elementos    → ~n iteraciones
+// 2n elementos   → ~2n iteraciones
+// 3n elementos   → ~3n iteraciones
+// El tiempo crece linealmente con el tamaño de la entrada.
+
+// Tiempo:  O(n)
+// Espacio: O(n)
